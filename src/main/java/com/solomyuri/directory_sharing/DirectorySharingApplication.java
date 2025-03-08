1b8ebe2d319de5b8bfb4be784d566a6e3a5debe2
solomyuri <solomyuri90@gmail.com>
2025-03-09 00:05:42 +0300

    add main logic

--- src/main/java/com/solomyuri/directory_sharing/DirectorySharingApplication.java
+++ src/main/java/com/solomyuri/directory_sharing/DirectorySharingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DirectorySharingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DirectorySharingApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(DirectorySharingApplication.class, args);
+    }
 
 }
--- src/main/java/com/solomyuri/directory_sharing/service/SharingServiceImpl.java
+++ src/main/java/com/solomyuri/directory_sharing/service/SharingServiceImpl.java
@@ -1,29 +1,62 @@
 package com.solomyuri.directory_sharing.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public class SharingServiceImpl implements SharingService {
 
+    private final String workingDirectory; 
+    
+    public SharingServiceImpl() {
+	this.workingDirectory = System.getProperty("user.dir");
+    }
+    
     @Override
     public List<String> getFiles() {
-	// TODO Auto-generated method stub
-	return null;
+	File dir = new File(workingDirectory);
+
+	if (!dir.exists() || !dir.isDirectory())
+	    return List.of();
+	else
+	    return Arrays.stream(dir.listFiles())
+	            .filter(File::isFile)
+	            .map(File::getName)
+	            .toList();
     }
 
     @Override
     public Resource download(String filename) {
-	// TODO Auto-generated method stub
-	return null;
+	File file = new File(workingDirectory + File.separator + filename);
+
+	if (file.exists() && file.isFile())
+	    return new FileSystemResource(file);
+	else
+	    return null;
     }
 
     @Override
-    public void upload(MultipartFile filel) {
-	// TODO Auto-generated method stub
-	
+    public void upload(MultipartFile file) {
+	if (!file.isEmpty()) {
+	    try {
+		Path path = Paths.get(workingDirectory + File.separator + file.getOriginalFilename());
+		Files.write(path, file.getBytes());
+	    } catch (IOException ex) {
+		Logger.getLogger("SharingServiceLogger").log(Level.WARNING, ex.getMessage());
+	    }
+	}
     }
-
 
 }
