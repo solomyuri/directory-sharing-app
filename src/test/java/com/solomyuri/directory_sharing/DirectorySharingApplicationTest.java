2c49c41e03b3d0f0f353e21f876034ffc65b299a
solomyuri <solomyuri90@gmail.com>
2025-03-08 19:26:45 +0300

    add Application

---
+++ src/test/java/com/solomyuri/directory_sharing/DirectorySharingApplicationTest.java
@@ -0,0 +1,13 @@
+package com.solomyuri.directory_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorySharingApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
